dfc0293c8ddd87223911c0e3fd8092d1de466ba1
nyanchan <personal@nyanchangames.com>
2021-04-17 12:05:25 +0900

    Add out of range and already check test

--- src/discard.ts
+++ src/discard.ts
@@ -16,14 +16,53 @@ class DiscardPairImple implements DiscardPair {
   }
 }
 
+// card check result
+export const CheckResult = {
+  SUCCESS: 0,
+  ALREADY_CHECKED: 1,
+  NOT_CHECKABLE: 2,
+} as const;
+export type CheckResult = typeof CheckResult[keyof typeof CheckResult];
+
+// card uncheck result
+export const UncheckResult = {
+  SUCCESS: 0,
+  ALREADY_UNCHECKED: 1,
+} as const;
+export type UncheckResult = typeof UncheckResult[keyof typeof UncheckResult];
+
 export class discardPlanner {
   private hand: Hand;
+  private lastDiscardPair: DiscardPair;
+  private strengthInverted: boolean;
   private checked: boolean[];
-  constructor(hand: Hand) {
+
+  constructor(
+    hand: Hand,
+    lastDiscardPair: DiscardPair,
+    strengthInverted: boolean
+  ) {
     this.hand = hand;
+    this.lastDiscardPair = lastDiscardPair;
+    this.strengthInverted = strengthInverted;
     this.checked = [];
     for (let i = 0; i < hand.count(); i++) {
       this.checked.push(false);
     }
+  }
+
+  public checkIfPossible(index: number): CheckResult {
+    // out of range?
+    if (index < 0 || index >= this.hand.count()) {
+      return CheckResult.NOT_CHECKABLE;
+    }
+
+    // already checked?
+    if (this.checked[index]) {
+      return CheckResult.ALREADY_CHECKED;
+    }
+
+    this.checked[index] = true;
+    return CheckResult.SUCCESS;
   }
 }
--- tests/discard.test.ts
+++ tests/discard.test.ts
@@ -1,10 +1,39 @@
+import * as Card from "../src/card";
 import * as Discard from "../src/discard";
 import { Hand } from "../src/hand";
+
+class DiscardPairMock implements Discard.DiscardPair {
+  cards: Card.Card[];
+  constructor() {
+    this.cards = [];
+  }
+}
 
 describe("DiscardPlanner", () => {
   it("can be instantiated", () => {
     const h = new Hand();
-    const p = new Discard.discardPlanner(h);
+    const d = new DiscardPairMock();
+    const p = new Discard.discardPlanner(h, d, false);
     expect(p).toBeTruthy();
+  });
+});
+
+describe("CheckIfPossible", () => {
+  it("returns NOT_CHECKABLE when index is out of range", () => {
+    const h = new Hand();
+    h.giveCards(new Card.Card(Card.Mark.SPADES, 3));
+    const d = new DiscardPairMock();
+    const p = new Discard.discardPlanner(h, d, false);
+    expect(p.checkIfPossible(-1)).toBe(Discard.CheckResult.NOT_CHECKABLE);
+    expect(p.checkIfPossible(1)).toBe(Discard.CheckResult.NOT_CHECKABLE);
+  });
+
+  it("returns ALREADY_CHECKED when the item is already checked", () => {
+    const h = new Hand();
+    h.giveCards(new Card.Card(Card.Mark.SPADES, 3));
+    const d = new DiscardPairMock();
+    const p = new Discard.discardPlanner(h, d, false);
+    expect(p.checkIfPossible(0)).toBe(Discard.CheckResult.SUCCESS);
+    expect(p.checkIfPossible(0)).toBe(Discard.CheckResult.ALREADY_CHECKED);
   });
 });
