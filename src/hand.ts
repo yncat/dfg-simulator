a57fceff09b883947afd6af29ae38a6b9b2d5b2a
nyanchan <personal@nyanchangames.com>
2021-04-18 16:21:30 +0900

    Add countSequencialCardsFrom

--- src/hand.ts
+++ src/hand.ts
@@ -2,6 +2,7 @@
 Player's hand
 */
 import * as Card from "./card";
+import * as CalcFunctions from "./calcFunctions";
 
 export class Hand {
   cards: Card.Card[];
@@ -29,6 +30,31 @@ export class Hand {
     return this.cards.filter((val) => {
       return val.isJoker();
     }).length;
+  }
+
+  public countSequencialCardsFrom(
+    cardNumber: number,
+    strengthInverted: boolean
+  ) {
+    // if this hand has 3 4 5 and the cardNumber parameter is 3, it will return 3 since we have 3 sequencial cards (3,4,5).
+    // when the strength is inverted, 7 6 5 and card parameter 7 will return 3.
+    let ret = 0;
+    let str = CalcFunctions.convertCardNumberIntoStrength(cardNumber);
+    while (true) {
+      if (str == 2 || str == 16) {
+        break;
+      }
+      if (
+        this.countCardsWithSpecifiedNumber(
+          CalcFunctions.convertStrengthIntoCardNumber(str)
+        ) == 0
+      ) {
+        break;
+      }
+      ret++;
+      str = strengthInverted ? str - 1 : str + 1;
+    }
+    return ret;
   }
 
   private sort() {
