1a439e430ea2a06e25af26f95fcf944ad8abc743
nyanchan <personal@nyanchangames.com>
2021-04-22 21:08:44 +0900

    Remove countSequencialCardsFrom from hand

--- src/hand.ts
+++ src/hand.ts
@@ -32,32 +32,6 @@ export class Hand {
     }).length;
   }
 
-  public countSequencialCardsFrom(
-    cardNumber: number,
-    strengthInverted: boolean
-  ) {
-    // TODO: move this to discard planner
-    // if this hand has 3 4 5 and the cardNumber parameter is 3, it will return 3 since we have 3 sequencial cards (3,4,5).
-    // when the strength is inverted, 7 6 5 and card parameter 7 will return 3.
-    let ret = 0;
-    let str = CalcFunctions.convertCardNumberIntoStrength(cardNumber);
-    while (true) {
-      if (str == 2 || str == 16) {
-        break;
-      }
-      if (
-        this.countCardsWithSpecifiedNumber(
-          CalcFunctions.convertStrengthIntoCardNumber(str)
-        ) == 0
-      ) {
-        break;
-      }
-      ret++;
-      str = strengthInverted ? str - 1 : str + 1;
-    }
-    return ret;
-  }
-
   private sort() {
     this.cards.sort((a, b) => {
       if (a.cardNumber == b.cardNumber) {
