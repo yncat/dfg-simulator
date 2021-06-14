1dc330028725bf8285fb3e884ef470e3198c6743
nyanchan <personal@nyanchangames.com>
2021-06-14 20:36:37 +0900

    Fix index test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,18 @@
 import * as Player from "../src/player";
 import * as Index from "../src/index";
 import * as Event from "../src/event";
+import * as Rule from "../src/rule";
 
 describe("createGame", () => {
   it("returns a new game instance and properly initializes related objects", () => {
     const p1 = Player.createPlayer("a");
     const p2 = Player.createPlayer("b");
     const p3 = Player.createPlayer("c");
-    const g = Index.createGame([p1, p2, p3], Event.createDefaultEventConfig());
+    const g = Index.createGame(
+      [p1, p2, p3],
+      Event.createDefaultEventConfig(),
+      Rule.createDefaultRuleConfig()
+    );
     expect(g).not.toBeNull();
     expect(p1.hand.count()).not.toBe(0);
     expect(p2.hand.count()).not.toBe(0);
@@ -21,7 +26,11 @@ describe("createGame", () => {
     const p2 = Player.createPlayer("b");
     const p3 = Player.createPlayer("b");
     expect(() => {
-      Index.createGame([p1, p2, p3], Event.createDefaultEventConfig());
+      Index.createGame(
+        [p1, p2, p3],
+        Event.createDefaultEventConfig(),
+        Rule.createDefaultRuleConfig()
+      );
     }).toThrow("one of the players' identifiers is duplicating");
   });
 });
