e647a539126e2d9ed4f8e7aaa9002db9354a2b37
Yukio Nozawa <personal@nyanchangames.com>
2021-06-29 23:56:21 +0900

    Adjust exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 export { CardMark, CardNumber } from "./card";
-export {
-  EventConfig,
-  createDefaultEventConfig,
-  EventDispatcher,
-  createEventDispatcher,
-} from "./event";
+export { EventReceiver } from "./event";
 export { DiscardPair } from "./discard";
 export { Game, createGame, GameCreationError } from "./game";
 export { generateUniqueIdentifiers } from "./player";
