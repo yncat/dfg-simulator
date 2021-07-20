af4b61b328f9d07d3a1a3956960c56c4cd3d30ba
Yukio Nozawa <personal@nyanchangames.com>
2021-07-21 00:59:00 +0900

    WIP discardPair unittest

--- tests/discardPair.test.ts
+++ tests/discardPair.test.ts
@@ -1,7 +1,48 @@
 import * as Discard from "../src/discard";
+import * as Card from "../src/card";
 
 describe("createNullDiscardPair", () => {
   it("returns null discardPair", () => {
     expect(Discard.createNullDiscardPair().isNull()).toBeTruthy();
+  });
+});
+
+describe("count", () => {
+  it("can count cards", () => {
+    const c1 = new Card.Card(Card.CardMark.CLUBS, 1);
+    const c2 = new Card.Card(Card.CardMark.CLUBS, 2);
+    const dp = Discard.CreateDiscardPairForTest(c1, c2);
+    expect(dp.count()).toBe(2);
+  });
+});
+
+describe("calcCardNumber", () => {
+  it("returns card number when same numbered pair", () => {
+    const c1 = new Card.Card(Card.CardMark.CLUBS, 1);
+    const dp = Discard.CreateDiscardPairForTest(c1, c1);
+    expect(dp.calcCardNumber(false)).toBe(1);
+  });
+
+  it("returns weakest card number when kaidan", () => {
+    const c1 = new Card.Card(Card.CardMark.CLUBS, 1);
+    const c2 = new Card.Card(Card.CardMark.CLUBS, 2);
+    const dp = Discard.CreateDiscardPairForTest(c1, c2);
+    expect(dp.calcCardNumber(false)).toBe(1);
+    expect(dp.calcCardNumber(true)).toBe(2);
+  });
+});
+
+describe("calcStrength", () => {
+  it("returns card strength when same numbered pair", () => {
+    const c1 = new Card.Card(Card.CardMark.CLUBS, 1);
+    const dp = Discard.CreateDiscardPairForTest(c1, c1);
+    expect(dp.calcStrength()).toBe(14);
+  });
+
+  it("returns weakest strength value when kaidan", () => {
+    const c1 = new Card.Card(Card.CardMark.CLUBS, 1);
+    const c2 = new Card.Card(Card.CardMark.CLUBS, 2);
+    const dp = Discard.CreateDiscardPairForTest(c1, c2);
+    expect(dp.calcStrength()).toBe(14);
   });
 });
